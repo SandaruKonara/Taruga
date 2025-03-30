5649a2617d5450bfd98d3892ea253197b5779e14
Sandaru Samintha Konara <it22180070@my.sliit.lk>
2025-03-30 22:43:40 +0530

    delete home tag

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ const Navbar = () => {
     <nav className="navbar">
       <h2>My Website</h2>
       <ul>
-        <li><Link to="/home">Home</Link></li>
+        <li><Link to="/">Home</Link></li>
         <li><Link to="/services">Services</Link></li>
         <li><Link to="/pricing">Pricing</Link></li>
         <li><Link to="/contact">Contact Us</Link></li>
